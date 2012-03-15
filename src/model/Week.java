c27fa9a8e0e1626d061ad1da799f31f7d5e2a07f
HulkingUnicorn <bjofoss2@hotmail.com>
2012-03-15 15:47:34 +0100

    Nye konstruktoerer i Week med validering.

--- src/model/Week.java
+++ src/model/Week.java
@@ -9,7 +9,56 @@ public class Week {
 	private ArrayList<Appointment> appointments;
 	private int weekNumber;
 	
+	//testing method
+	public static void main(String[] args) {
+		Calendar c = Calendar.getInstance();
+		c.set(2012, 2, 12);
+		Date start = c.getTime();
+		System.out.println(start);
+		new Week(start);
+	}
 	
+	/**Create a week of correct duration
+	 * @param startDate Date of the monday
+	 */
+	public Week(Date startDate) {
+		this.startDate = startDate;
+		Calendar d = Calendar.getInstance();
+		d.setFirstDayOfWeek(Calendar.MONDAY);
+		d.setTime(this.startDate);
+		if (d.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+			throw new IllegalArgumentException("Chosen day is not a monday!");
+		}
+		int maxday = d.getActualMaximum(Calendar.DAY_OF_WEEK);
+		d.set(Calendar.DAY_OF_WEEK, maxday + 1);
+		this.endDate = d.getTime();
+		this.weekNumber = calcWeekNumber(startDate);
+		this.appointments = new ArrayList<Appointment>();
+	}
+	
+	/**Create a week of correct duration with the
+	 * specified appointments.
+	 * @param startDate Date of the monday
+	 * @param appointments List of appointments
+	 */
+	public Week(Date startDate, ArrayList<Appointment> appointments) {
+		this.startDate = startDate;
+		Calendar d = Calendar.getInstance();
+		d.setFirstDayOfWeek(Calendar.MONDAY);
+		d.setTime(this.startDate);
+		if (d.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+			throw new IllegalArgumentException("Chosen day is not a monday!");
+		}
+		int maxday = d.getActualMaximum(Calendar.DAY_OF_WEEK);
+		d.set(Calendar.DAY_OF_WEEK, maxday + 1);
+		this.endDate = d.getTime();
+		this.weekNumber = calcWeekNumber(startDate);
+		this.appointments = appointments;
+	}
+	
+	/**
+	 * Create a week with start and finish dates.
+	 */
 	public Week(Date startDate, Date endDate) {
 		this.startDate = startDate;
 		this.endDate = endDate;
@@ -19,6 +68,10 @@ public class Week {
 		}
 	}
 	
+	/**
+	 * Create a week with start-, finish dates and
+	 * a list of appointments for the week.
+	 */
 	public Week(Date startDate, Date endDate, ArrayList<Appointment> appointments) {
 		this.startDate = startDate;
 		this.endDate = endDate;
@@ -29,12 +82,18 @@ public class Week {
 		}
 	}
 
+	/**
+	 * Get the correct week number for the specified date
+	 */
 	private static int calcWeekNumber(Date date) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		return cal.get(Calendar.WEEK_OF_YEAR);
 	}
 	
+	/**
+	 * Get the week number for this week.
+	 */
 	public int getWeekNumber() {
 		return this.weekNumber;
 	}
@@ -50,5 +109,17 @@ public class Week {
 	@SuppressWarnings("unchecked")
 	public ArrayList<Appointment> getAppointments() {
 		return  (ArrayList<Appointment>) this.appointments.clone();
+	}
+	
+	public void setAppointments(ArrayList<Appointment> appointments) {
+		this.appointments = appointments;
+	}
+	
+	public void addAppointment(Appointment app) {
+		this.appointments.add(app);
+	}
+	
+	public void removeAppointment(Appointment app) {
+		this.appointments.remove(app);
 	}
 }
