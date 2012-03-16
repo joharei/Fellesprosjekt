890591a4b015df261c211a8f830c37223fc06249
Stian Weie <stianwe@gmail.com>
2012-03-16 13:41:52 +0100

    Startet paa behandling av user connections

--- src/synclogic/ServerSynchronizationUnit.java
+++ src/synclogic/ServerSynchronizationUnit.java
@@ -1,21 +1,35 @@
 package synclogic;
 
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 import model.User;
 import no.ntnu.fp.net.co.Connection;
+import no.ntnu.fp.net.co.ConnectionImpl;
 import nu.xom.Document;
 
 public class ServerSynchronizationUnit extends SynchronizationUnit {
 
 	private Queue<Document> sendQueue, receiveQueue;
-	private List<Connection> connections;
-	private List<User> activeUsers;
+//	private List<Connection> connections;
+//	private List<User> activeUsers;
+	private Map<Connection, User> activeUserConnections;
 	private DatabaseUnit dbUnit;
+	/**
+	 * The connection that will be used to listen for incomming connections
+	 */
+	private Connection connection;
 	
 	public ServerSynchronizationUnit() {
 		super();
+		this.activeUserConnections = new HashMap<Connection, User>();
+		this.sendQueue = new LinkedList<Document>();
+		this.receiveQueue = new LinkedList<Document>();
 	}
 
 	@Override
@@ -24,5 +38,22 @@ public class ServerSynchronizationUnit extends SynchronizationUnit {
 		throw new RuntimeException("NOT YET IMPLEMENTED!!");
 	}
 
+	public void listenForUserConnections(int port) {
+		this.connection = new ConnectionImpl(port);
+		while(true) {
+			try {
+				startUserSession(this.connection.accept());
+			} catch (SocketTimeoutException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 	
+	private void startUserSession(Connection con) {
+		
+	}
 }
