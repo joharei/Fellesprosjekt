0dd7a406a9bedb731a3844f3d2b4e41b339b53fa
HulkingUnicorn <bjofoss2@hotmail.com>
2012-03-16 12:26:40 +0100

    Lagt til getters/setters/adders/removers

--- src/model/Meeting.java
+++ src/model/Meeting.java
@@ -10,4 +10,38 @@ public class Meeting extends Appointment {
 	public static final String NAME_PROPERTY_CLASSTYPE = "meeting";
 	public static final String NAME_PROPERTY_PARTICIPANTS = "participants";
 	public static final String NAME_PROPERTY_INVITATIONS = "invitations";
+	
+	public void addParticipant(User participant) {
+		this.participants.add(participant);
+	}
+	
+	public void removeParticipant(User participant) {
+		this.participants.remove(participant);
+	}
+	
+	public void setParticipants(ArrayList<User> participants) {
+		this.participants = participants;
+	}
+	
+	public void addInvitation(Invitation inv) {
+		this.invitations.add(inv);
+	}
+	
+	public void removeInvitation(Invitation inv) {
+		this.invitations.remove(inv);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public ArrayList<User> getParticipants() {
+		return (ArrayList<User>) participants.clone();
+	}
+
+	public void setInvitations(ArrayList<Invitation> invitations) {
+		this.invitations = invitations;
+	}
+
+	@SuppressWarnings("unchecked")
+	public ArrayList<Invitation> getInvitations() {
+		return (ArrayList<Invitation>) invitations.clone();
+	}
 }
