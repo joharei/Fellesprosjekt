7002b23036921d122b1cf16f0797dd42194ee143
Hanne Kottum <hannekottum@gmail.com>
2012-03-16 14:00:28 +0100

    smaa endringer

--- src/synclogic/DatabaseUnit.java
+++ src/synclogic/DatabaseUnit.java
@@ -32,16 +32,16 @@ public class DatabaseUnit {
 		if(object instanceof User){
 			User user = (User)object;
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT Username FROM User");
+			ResultSet rs = stmt.executeQuery("SELECT Username FROM User WHERE Username='" + user.getUsername() +"'");
 			while (rs.next()) {
-				if(user.getUsername().equals(rs.getString("Username"))){
+			//	if(user.getUsername().equals(rs.getString("Username"))){
 					Statement update = conn.createStatement();
 					ResultSet rs2 = update.executeQuery("UPDATE User SET Passord='" +user.getPassword()+"', Email='"
 							+user.getEmail() +"', DateOfBirth='" +user.getDateOfBirth() + "', Phone ='" 
 							+user.getPhone() +"', SurName='" +user.getSurname() +"'FirstName='" +user.getFirstname()+
 							"WHERE Username='" +user.getUsername() + "'") ;
 					return;
-				}
+			//	}
 			}
 			Statement insertInto = conn.createStatement();
 			ResultSet rs1 = insertInto.executeQuery("INSERT INTO User VALUES(" + user.getUsername()+","
@@ -51,31 +51,31 @@ public class DatabaseUnit {
 		else if(object instanceof Room){
 			Room room = (Room) object;
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT RoomID FROM Room");
+			ResultSet rs = stmt.executeQuery("SELECT RoomID FROM Room WHERE RoomID='" + room.getId() + "'");
 			while(rs.next()){
-				if(room.getId() == rs.getInt("RoomID")){
+			//	if(room.getId() == rs.getInt("RoomID")){
 					Statement update = conn.createStatement();
 					ResultSet rs2 = update.executeQuery("UPDATE Room SET Name='" + room.getName() + "', Capacity='" 
 							+ room.getCapacity()+"' WHERE RoomID='" + room.getId() +"'");
 					return;
-				}
+				//}
+			}
 				Statement insertInto = conn.createStatement();
 				ResultSet rs1 = insertInto.executeQuery("INSERT INTO Room VALUES(" + room.getId() + "," 
 						+ room.getName() + "'" + room.getCapacity() + ")");
 			}
-		}
 		else if(object instanceof Appointment){
 			Appointment appment = (Appointment) object;
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT AID FROM Appointment");
+			ResultSet rs = stmt.executeQuery("SELECT AID FROM Appointment WHERE AID='" + appment.getId() +"'" );
 			while(rs.next()){
-				if(appment.getId() == rs.getInt("AID")){
+			//	if(appment.getId() == rs.getInt("AID")){
 					Statement update = conn.createStatement();
 					ResultSet rs2 = update.executeQuery("UPDATE Appointment SET Date='" + appment.getDate() + "', Start='" 
 							+appment.getStartTime() +"', End='" + appment.getEndTime() + "', Description='" 
 							+ appment.getDescription() + "', Location='" + appment.getLocation() +"' WHERE AID='" +appment.getId() + "'");
 					return;
-				}
+			//	}
 			}
 			Statement insertInto = conn.createStatement();
 			ResultSet rs1 = insertInto.executeQuery("INSERT INTO Appointment VALUES(" +appment.getId() +"," 
@@ -85,14 +85,14 @@ public class DatabaseUnit {
 		else if(object instanceof Notification){
 			Notification not= (Notification) object;
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT NotificationID FROM Notification");
+			ResultSet rs = stmt.executeQuery("SELECT NotificationID FROM Notification WHERE NOtficationID='" +not.getId() + "'");
 			while(rs.next()){
-				if(not.getId() == rs.getInt("NotificationID")){
+				//if(not.getId() == rs.getInt("NotificationID")){
 					Statement update = conn.createStatement();
 					ResultSet rs2 = update.executeQuery("UPDATE Notification SET Type='" + not.getType() 
 							+"' WHERE NotificationID='" + not.getId() +"'");
 					return;
-				}
+				//}
 			}
 			Statement insertInto = conn.createStatement();
 			ResultSet rs1 = insertInto.executeQuery("INSERT INTO Notification VALUES(" + not.getId() +"," 
