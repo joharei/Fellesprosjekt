2a7c831f34c54ba31f7dcb9bd07a2a223fb46d27
Hanne Kottum <hannekottum@gmail.com>
2012-03-20 09:51:14 +0100

    oppdatert objectToDb

--- src/synclogic/DatabaseUnit.java
+++ src/synclogic/DatabaseUnit.java
@@ -13,102 +13,87 @@ import model.Room;
 import model.User;
 
 public class DatabaseUnit {
-	private String username;
-	private String password;
-	private String url;
-	private Connection conn = null;
-	public Connection dbConnect() throws ClassNotFoundException, SQLException{
-		Class.forName("loadJDBCDriver");  //en adresse
+
+	private static Connection conn = null;
 	
-		Properties connectionProps = new Properties();
-		connectionProps.put("user", username);
-		connectionProps.put("password", password);
-		conn = DriverManager.getConnection(url, connectionProps);
-		
-		return conn;
+	public void dbConnect() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException{
+		Properties props = new Properties();
+		props.put("user","hannekot_26");
+		props.put("password","gruppe26");
+			
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		conn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/hannekot_X-cal", props);
 	}
 	
-	public void objectToDB(Object object) throws SQLException{
+	public static void objectToDb(Object object) throws SQLException{
 		if(object instanceof User){
-			User user = (User)object;
+			User user1 = (User)object;
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT Username FROM User WHERE Username='" + user.getUsername() +"'");
+			ResultSet rs = stmt.executeQuery("SELECT Username FROM User WHERE Username='" + user1.getUsername() +"'");
+			String dateOfBirth = (User.getDateFormat().format(user1.getDateOfBirth()));
 			while (rs.next()) {
-			//	if(user.getUsername().equals(rs.getString("Username"))){
 					Statement update = conn.createStatement();
-					ResultSet rs2 = update.executeQuery("UPDATE User SET Passord='" +user.getPassword()+"', Email='"
-							+user.getEmail() +"', DateOfBirth='" +user.getDateOfBirth() + "', Phone ='" 
-							+user.getPhone() +"', SurName='" +user.getSurname() +"'FirstName='" +user.getFirstname()+
-							"WHERE Username='" +user.getUsername() + "'") ;
+					update.executeUpdate("UPDATE User SET Password='" +user1.getPassword()+"', Email='"
+							+user1.getEmail() +"', DateOfBirth='" + dateOfBirth + "', Phone='" 
+							+user1.getPhone() +"', Surname='" +user1.getSurname() +"', Firstname='" +user1.getFirstname()
+							+ "' WHERE Username='" +user1.getUsername() + "';") ;
 					return;
-			//	}
-			}
+			}	
 			Statement insertInto = conn.createStatement();
-			ResultSet rs1 = insertInto.executeQuery("INSERT INTO User VALUES(" + user.getUsername()+","
-					+ user.getPassword() +"," + user.getEmail() + "," + user.getDateOfBirth() + "'"
-					+ user.getPhone() + "'" + user.getSurname() + "'" + user.getFirstname() + ")");
+			insertInto.executeUpdate("INSERT INTO User VALUES('" + user1.getUsername()+"','"
+					+ user1.getPassword() +"','" + user1.getEmail() + "','" + dateOfBirth + "','"
+					+ user1.getPhone() + "','" + user1.getSurname() + "','" + user1.getFirstname() + "','0');");
 		}
 		else if(object instanceof Room){
 			Room room = (Room) object;
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT RoomID FROM Room WHERE RoomID='" + room.getId() + "'");
+			ResultSet rs = stmt.executeQuery("SELECT RoomID FROM Room WHERE RoomID='" + room.getId()+ "'");
 			while(rs.next()){
-			//	if(room.getId() == rs.getInt("RoomID")){
 					Statement update = conn.createStatement();
-					ResultSet rs2 = update.executeQuery("UPDATE Room SET Name='" + room.getName() + "', Capacity='" 
-							+ room.getCapacity()+"' WHERE RoomID='" + room.getId() +"'");
+					update.executeUpdate("UPDATE Room SET Name='" + room.getName()+ "', Capacity='" + room.getCapacity()
+								+ "' WHERE RoomID='" +room.getId() + "'");
 					return;
-				//}
-			}
-				Statement insertInto = conn.createStatement();
-				ResultSet rs1 = insertInto.executeQuery("INSERT INTO Room VALUES(" + room.getId() + "," 
-						+ room.getName() + "'" + room.getCapacity() + ")");
-			}
-		else if(object instanceof Appointment){
-			Appointment appment = (Appointment) object;
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT AID FROM Appointment WHERE AID='" + appment.getId() +"'" );
-			while(rs.next()){
-			//	if(appment.getId() == rs.getInt("AID")){
-					Statement update = conn.createStatement();
-					ResultSet rs2 = update.executeQuery("UPDATE Appointment SET Date='" + appment.getDate() + "', Start='" 
-							+appment.getStartTime() +"', End='" + appment.getEndTime() + "', Description='" 
-							+ appment.getDescription() + "', Location='" + appment.getLocation() +"' WHERE AID='" +appment.getId() + "'");
-					return;
-			//	}
 			}
 			Statement insertInto = conn.createStatement();
-			ResultSet rs1 = insertInto.executeQuery("INSERT INTO Appointment VALUES(" +appment.getId() +"," 
-					+ appment.getDate() + "," + appment.getStartTime() +"," +appment.getEndTime() +"," 
-					+ appment.getDescription() +"," + appment.getLocation() + ")" );
-		}
+			insertInto.executeUpdate("INSERT INTO Room (Name, Capacity) VALUES('" + room.getName() + "','" + room.getCapacity() + "');");
+			}
+
 		else if(object instanceof Notification){
 			Notification not= (Notification) object;
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT NotificationID FROM Notification WHERE NOtficationID='" +not.getId() + "'");
 			while(rs.next()){
-				//if(not.getId() == rs.getInt("NotificationID")){
 					Statement update = conn.createStatement();
 					ResultSet rs2 = update.executeQuery("UPDATE Notification SET Type='" + not.getType() 
 							+"' WHERE NotificationID='" + not.getId() +"'");
 					return;
-				//}
 			}
 			Statement insertInto = conn.createStatement();
 			ResultSet rs1 = insertInto.executeQuery("INSERT INTO Notification VALUES(" + not.getId() +"," 
 					+ not.getType() +")");
 		}
+	//	conn.close();
+	}
 		
 		
 		//sjekke om objekte ligger i databasen  object instance of User
 		//hvis ja: oppdater databasen med ny informasjon om objectet
 		//hvis nei: Legge til det nye objektet i databasen
-	}
 	
 	public void load(){
 		//når servern starter så hentes alle objektene ut fra databasen og lage objektene som ligger i databasen 
 		//så for hver user i databasen skal det lages et User objekt ved hjelp av user-klassen sin konstruktør.
+		//lage mindre små metoder for User, Event, Notification osv, for så å kalle alle metodene i denne
+		//legge de ferdig laga objektene i en liste
 		
 		
 	} 
+	
+	
+	
+	public void closeConnection() throws SQLException
+	{
+		conn.close();
+	}
+
 }
