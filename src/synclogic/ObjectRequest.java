43d2e36be466c7122b76c64d4b0761cdce2dd3a7
Stian Weie <stianwe@gmail.com>
2012-03-20 11:08:50 +0100

    La til javadoc og endret et metodenavn

--- src/synclogic/ObjectRequest.java
+++ src/synclogic/ObjectRequest.java
@@ -7,16 +7,28 @@ public class ObjectRequest {
 	private SaveableClass c;
 	private String id;
 	
+	/**
+	 * Creates a new ObjectRequest with the given arguments
+	 * 
+	 * @param c		The requested object's class
+	 * @param id	The requested object's id, or null if all objects of this class is requested
+	 */
 	public ObjectRequest(SaveableClass c, String id) {
 		this.c = c;
 		this.id = id;
 	}
 	
+	/**
+	 * @return	The requested object's class
+	 */
 	public SaveableClass getSaveableClass() {
 		return this.c;
 	}
 	
-	public String getID() {
+	/**
+	 * @return The ID of the object, or null if all objects of the given class is requested.
+	 */
+	public String getObjectID() {
 		return this.id;
 	}
 }
