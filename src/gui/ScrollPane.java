b995c522e948835e4c6cfdccbcd957233932620b
Kongshem <magnus@kongshem.com>
2012-03-20 11:29:38 +0100

    mmmm

---
+++ src/gui/ScrollPane.java
@@ -0,0 +1,17 @@
+package gui;
+
+import java.awt.Dimension;
+
+import javax.swing.JScrollPane;
+
+public class ScrollPane extends JScrollPane{
+	
+	private String [] days = { "Time"," - ","Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"};
+	
+	public ScrollPane(){
+		super(new GUICalender());
+		setVisible(true);
+		setPreferredSize(new Dimension(400,300));
+		
+	}
+}
