87b021b797bcb0916bb6999ef3243ae3d9c4f511
Stian Weie <stianwe@gmail.com>
2012-03-20 13:32:26 +0100

    Endret returtypen til fire

--- src/synclogic/SyncListener.java
+++ src/synclogic/SyncListener.java
@@ -9,9 +9,8 @@ public interface SyncListener {
 	 * 
 	 * @param classType		The type of the object that has changed		
 	 * @param newVersion	A copy of the object with the changes
-	 * @return				True if the object exists. False if not
 	 */
-	public boolean fire(SaveableClass classType, Object newVersion);
+	public void fire(SaveableClass classType, Object newVersion);
 	
 	public SaveableClass getSaveableClass();
 	
