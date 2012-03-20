2188ca7cdfbab72af19430c1fa8c8e0e595fe642
Stian Weie <stianwe@gmail.com>
2012-03-20 13:20:57 +0100

    Opprettet UpdateRequest med noen enkle metoder, og fikset paa javadocen i SyncListener

--- src/synclogic/SyncListener.java
+++ src/synclogic/SyncListener.java
@@ -9,7 +9,7 @@ public interface SyncListener {
 	 * 
 	 * @param classType		The type of the object that has changed		
 	 * @param newVersion	A copy of the object with the changes
-	 * @return
+	 * @return				True if the object exists. False if not
 	 */
 	public boolean fire(SaveableClass classType, Object newVersion);
 	
---
+++ src/synclogic/UpdateRequest.java
@@ -0,0 +1,25 @@
+package synclogic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UpdateRequest {
+
+	private List<SyncListener> objects;
+	
+	public UpdateRequest() {
+		this.objects = new ArrayList<SyncListener>();
+	}
+	
+	public void addObject(SyncListener o) {
+		this.objects.add(o);
+	}
+	
+	public int size() {
+		return objects.size();
+	}
+	
+	public SyncListener getObject(int i) {
+		return this.objects.get(i);
+	}
+}
