d09f01998c345a1e102e45f4bb7999c292b73427
HulkingUnicorn <bjofoss2@hotmail.com>
2012-03-21 13:20:56 +0100

    Endring modeller, hovedsaklig pcs
    * Endre felt for id i Notification, inkludert konstruktoerer osv
    * Legg til stoette for pcs i:
    	* Appointment
    	* Notification
    	* Room
    	* Week
    * Rette pcs i User

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeSupport;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,6 +17,7 @@ public class Appointment implements SyncListener {
 	private boolean isDeleted;
 	private static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 	private static DateFormat timeformat = new SimpleDateFormat("HH:mm:ss");
+	private PropertyChangeSupport pcs;
 	
 	//constants
 	public static final String NAME_PROPERTY_CLASSTYPE = "appointment";
@@ -39,58 +42,77 @@ public class Appointment implements SyncListener {
 		setId(id);
 		setOwner(owner);
 		setDeleted(isDeleted);
+		pcs = new PropertyChangeSupport(this);
 	}
 	
 	public Date getDate() {
 		return date;
 	}
 	public void setDate(Date date) {
+		Date old = getDate();
 		this.date = date;
+		pcs.firePropertyChange(new PropertyChangeEvent(this, NAME_PROPERTY_DATE, old, date));
 	}
 	public Date getStartTime() {
 		return startTime;
 	}
 	public void setStartTime(Date startTime) {
+		Date old = getStartTime();
 		this.startTime = startTime;
+		pcs.firePropertyChange(new PropertyChangeEvent(this, NAME_PROPERTY_START_TIME, old, startTime));
 	}
 	public Date getEndTime() {
 		return endTime;
 	}
 	public void setEndTime(Date endTime) {
+		Date old = getEndTime();
 		this.endTime = endTime;
+		pcs.firePropertyChange(new PropertyChangeEvent(this, NAME_PROPERTY_END_TIME, old, endTime));
 	}
 	public String getDescription() {
 		return description;
 	}
 	public void setDescription(String description) {
+		String old = getDescription();
 		this.description = description;
+		pcs.firePropertyChange(new PropertyChangeEvent(this, NAME_PROPERTY_DESCRIPTION, old, description));
 	}
 	public String getLocation() {
 		return location;
 	}
 	public void setLocation(String location) {
+		String old = getLocation();
 		this.location = location;
+		pcs.firePropertyChange(new PropertyChangeEvent(this, NAME_PROPERTY_LOCATION, old, location));
 	}
 	public Room getRoom() {
 		return room;
 	}
 	public void setRoom(Room room) {
+		Room old = getRoom();
 		this.room = room;
+		pcs.firePropertyChange(new PropertyChangeEvent(this, NAME_PROPERTY_ROOM, old, room));
 	}
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
+		int old = getId();
 		this.id = id;
+		pcs.firePropertyChange(new PropertyChangeEvent(this, NAME_PROPERTY_ID, old, id));
 	}
 	public User getOwner() {
 		return owner;
 	}
 	public void setOwner(User owner) {
+		User old = getOwner();
 		this.owner = owner;
+		pcs.firePropertyChange(new PropertyChangeEvent(this, NAME_PROPERTY_OWNER, old, owner));
 	}
 	public void setDeleted(boolean isDeleted) {
+		boolean old = isDeleted();
 		this.isDeleted = isDeleted;
+		pcs.firePropertyChange(new PropertyChangeEvent(this, NAME_PROPERTY_DELETED, old, isDeleted));
 	}
 	public boolean isDeleted() {
 		return isDeleted;
