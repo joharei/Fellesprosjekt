fa1bc44eab5faa45c216cf2eac73468d3904fd80
Kongshem <magnus@kongshem.com>
2012-03-21 10:09:33 +0100

    endringer i notification

--- src/gui/GUINotification.java
+++ src/gui/GUINotification.java
@@ -2,5 +2,18 @@ package gui;
 
 public class GUINotification {
 	
+	public GUINotification(){
+		
+//		String message2= "Invitation";
+//		String message3= "Meeting declined";
+//		String message4= "Meeting accepted";
+		
+	}
+	
+	public String [] sendNotification(){
+		String message []={"Meeting deleted","Invitation","Meeting declined","Meeting accepted"};
+		
+		return message;
+	}
 	
 }
