b51500070cd92fe919bc9b1fdd8f138d6ad36e7e
HulkingUnicorn <bjofoss2@hotmail.com>
2012-03-21 09:41:44 +0100

    Endringer invitation
    * Fjern notat om endringer klassediagram, ikke noedvendig.
    * Fjern ubrukt konstant, attributt og getter/setter

--- src/model/Invitation.java
+++ src/model/Invitation.java
@@ -5,22 +5,18 @@ import synclogic.SyncListener;
 public class Invitation implements SyncListener {
 	private InvitationStatus status;
 	private Meeting meeting;
-	private User invitee;//TODO: Update class diagram
 	private String id;
 	
 	//constants
 	public static final String NAME_PROPERTY_CLASSTYPE = "invitation";
 	public static final String NAME_PROPERTY_STATUS = "status";
 	public static final String NAME_PROPERTY_MEETING = "meeting";
-	public static final String NAME_PROPERTY_INVITEE = "invitee";
 	
 	public Invitation(InvitationStatus status, Meeting meeting, String id) {
 		setStatus(status);
 		setMeeting(meeting);
 		setID(id);
 	}
-	
-//TODO: Update class diagram, remove invitee
 	
 	public void setID(String id) {
 		this.id = id;
@@ -45,19 +41,10 @@ public class Invitation implements SyncListener {
 		return meeting;
 	}
 
-	public void setInvitee(User invitee) {
-		this.invitee = invitee;
-	}
-
-	public User getInvitee() {
-		return invitee;
-	}
-
 	@Override
 	public void fire(SaveableClass classType, Object newVersion) {
 		Invitation inv = (Invitation) newVersion;
 		setID(inv.getID());
-		setInvitee(inv.getInvitee());
 		setMeeting(inv.getMeeting());
 		setStatus(inv.getStatus());
 		System.out.println("Invitation updated!");
