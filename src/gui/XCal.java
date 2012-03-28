f05df0c4acebe743e2af3cbb72ded165a1b6f0b9
Johan Reitan <johan.reitan@gmail.com>
2012-03-28 09:38:05 +0200

    La til XCal igjen!

---
+++ src/gui/XCal.java
@@ -0,0 +1,28 @@
+package gui;
+
+
+import javax.swing.JTextField;
+
+import synclogic.ClientSynchronizationUnit;
+
+public class XCal {
+	
+	private static ClientSynchronizationUnit CSU;
+	static JTextField usernameField;
+	private static String serverIp = "78.91.83.107";
+//	private static String serverIp = "localhost";
+	public static void main(String[] args) {
+		CSU=new ClientSynchronizationUnit();
+		LogIn logIn = new LogIn();
+		logIn.setVisible(true);
+	}
+
+	public static ClientSynchronizationUnit getCSU() {
+		return CSU;
+	}
+	
+	public static String getServerIp(){
+		return serverIp;
+	}
+	
+}
