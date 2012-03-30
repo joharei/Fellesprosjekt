81a7a5a321cdb8e2043b938309e3b8b8991aa8ae
HulkingUnicorn <bjofoss2@hotmail.com>
2012-03-31 01:19:45 +0200

    Sett ip localhost

--- src/gui/XCal.java
+++ src/gui/XCal.java
@@ -9,8 +9,8 @@ public class XCal {
 	
 	private static ClientSynchronizationUnit CSU;
 	public static JTextField usernameField;
-	private static String serverIp = "78.91.83.107";
-//	private static String serverIp = "localhost";
+//	private static String serverIp = "78.91.83.107";
+	private static String serverIp = "localhost";
 	public static void main(String[] args) {
 		CSU=new ClientSynchronizationUnit();
 		LogIn logIn = new LogIn();
